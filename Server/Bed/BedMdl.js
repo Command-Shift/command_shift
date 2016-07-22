c9124d41586da69edd0b285da3224e70d8b01a01
Micky Shaked <micky.shaked@gmail.com>
2016-07-22 15:49:42 -0700

    bed model/ctrl added, nurse ctrl/model updated, server routes added

---
+++ Server/Bed/BedMdl.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/db');
+
+const bedSchema = new Schema({
+  'bed': String,
+  'status': {type: boolean, default: true}
+  'notes': {type: String, default: null}
+});
+
+const bed = mongoose.model('bed', bedSchema);
+
+module.exports = bed;
--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const nurseCtrl = require('./Nurse/nurseCtrl');
+const bedCtrl = require('./Bed/bedCtrl');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -13,7 +14,11 @@ app.get('/', (req, res) => {
 
 app.delete('/nurse', nurseCtrl.remove);
 app.get('/nurses', nurseCtrl.index);
-app.post('/nurse', nurseCtrl.post);
+app.post('/nurse', nurseCtrl.add);
+app.post('/changeBed', bedCtrl.changeBed, nurseCtrl.changeBed);
+app.post('/addBeds', bedCtrl.addBeds);
+app.post('/emptyBeds', bedCtrol.emptyBeds);
+app.post('/assign', nurseCtrl.assign);
 
 
 app.listen(3000, function () {
