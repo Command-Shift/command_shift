525adc688ff3581c5d25d2bf45a9e6b00243da0c
Chandra Lindy <chandra.lindy@gmail.com>
2016-07-22 23:55:45 -0700

    admit and discharge feature functioning

--- Client/Admin_client/Components/assign.jsx
+++ Client/Admin_client/Components/assign.jsx
@@ -8,12 +8,12 @@ export default class Assign extends Component {
       <div className="container">
         {this.props.assignment.map((ar, i) => {
           return (
-            <div>
+            <div className="assignment" key={i}>
               <div className="header">
                 <span className="name" key={i}>{this.props.nurses[i]}</span>
               </div>
               <div className="body">
-                <Run run={ar} key={i}/>
+                <Run run={ar} key={i} />
               </div>
             </div>
           );
--- Client/Admin_client/app.jsx
+++ Client/Admin_client/app.jsx
@@ -17,6 +17,8 @@ class App extends Component {
     this.assign = this.assign.bind(this);
     this.add = this.add.bind(this);
     this.remove = this.remove.bind(this);
+    this.admit = this.admit.bind(this);
+    this.discharge = this.discharge.bind(this);
 
     this.state = {
       beds: ['2', '4', '6', '8A', '8B', '1A', '1B', '10A', '10B', '3A',
@@ -49,32 +51,18 @@ class App extends Component {
   }
 
   enter(event) {
-    function removeFrom(emptyBeds, beds) {
-      return beds.filter(el => emptyBeds.indexOf(el) < 0);
-    }
+    // function removeFrom(emptyBeds, beds) {
+    //   return beds.filter(el => emptyBeds.indexOf(el) < 0);
+    // }
 
-    if (event.keyCode === 13) { // refactor to switch
+    if (event.keyCode === 13) {
       const value = event.target.value;
-      if (value.slice(0, 3) === 'add') {
+      if (value.slice(0, 3) === 'add') { // inserts a new nurse in db
         event.target.value = '';
         this.add(value);
-      } else if (value.slice(0, 5) === 'empty') {
-        const emptyBeds = value.toUpperCase().split(' ');
-        emptyBeds.shift();
-        const occupied = removeFrom(emptyBeds, [...this.state.beds]);
-        const census = occupied.length;
-        $.ajax({
-          method: 'POST',
-          url: '/emptyBeds',
-          data: emptyBeds
-        });
-        this.setState({
-          occupied,
-          census,
-          emptyBeds,
-          view: 'display'
-        });
+      } else if (value.slice(0, 9) === 'discharge') {
         event.target.value = '';
+        this.discharge(value);
       } else if (value === 'clear') {
         event.target.value = '';
         this.reset();
@@ -91,21 +79,46 @@ class App extends Component {
           url: '/populate',
           data: beds
         });
+      } else if (value.slice(0, 5) === 'admit') {
+        event.target.value = '';
+        this.admit(value);
       } else {
         event.target.value = '';
-        this.setState({view: value});
+        this.setState({ view: value });
       }
     }
   }
 
-  assign() {
-    const nurses = [...this.state.onduty];
+  discharge(value) {
+    const arr = value.toUpperCase().split(' ');
+    arr.shift();
+    console.log('arr: ', arr);
     $.ajax({
       method: 'POST',
+      url: '/emptyBeds',
+      data: { emptyBeds: arr },
+    }).then(data => console.log('success: ', data));
+  }
+
+  admit(value) {
+    const arr = value.toUpperCase().split(' ');
+    arr.shift();
+    $.ajax({
+      method: 'POST',
+      url: '/addBeds',
+      data: { addBeds: arr },
+    });
+  }
+
+  assign() {
+    const nurses = [...this.state.onduty];
+    const post = $.ajax({
+      method: 'POST',
       url: '/assign',
-      data: { onDuty: nurses }
-    }).then((data) => {
-      this.setState({ onduty: nurses, occupied: data, view: 'assign' });
+      data: { onDuty: nurses },
+    });
+    post.then((data) => {
+      this.setState({ onduty: data.onDuty, assignment: data.assignment, view: 'assign' });
     });
   }
 
@@ -115,15 +128,15 @@ class App extends Component {
   }
 
   add(value) {
-    value = value.split(' ');
+    const input = value.split(' ');
     const obj = {
-      first: value[1],
-      last: value[2]
+      first: input[1],
+      last: input[2],
     };
     const post = $.ajax({
       method: 'POST',
       url: '/nurse',
-      data: obj
+      data: obj,
     });
     post.then(() => {
       this.refresh();
@@ -131,15 +144,15 @@ class App extends Component {
   }
 
   remove(value) {
-    value = value.split(' ');
+    const input = value.split(' ');
     const obj = {
-      first: value[1],
-      last: value[2]
+      first: input[1],
+      last: input[2],
     };
     const post = $.ajax({
       method: 'DELETE',
       url: '/nurse',
-      data: obj
+      data: obj,
     });
     post.then(() => {
       this.refresh();
@@ -149,22 +162,22 @@ class App extends Component {
   reset() {
     $.get('/nurses').then((data) => {
       this.setState({
-        beds: ['2','4','6','8A','8B','1A','1B','10A','10B','3A'
-        ,'3B','12A','12B','5A','5B','14A','14B','7A','7B','16A','16B'
-        ,'9A','9B','18A','18B','11A','11B','20A','20B','22A','22B','22C'
-        ,'22D','15A','15B','15C','17A','17B','19A','19B','21A','21B','24A'
-        ,'24B','24C','23A','23B','26A','26B','26C','25A','25B','27A','27B'
-        ,'29A','29B','31A','31B','28A','28B','28C','30A','30B','33A','33B'
-        ,'32A','32B','35A','35B','34A','34B','37A','37B','36A','36B','39A'
-        ,'39B','38A','38B','41A','41B','40A','40B','43A','43B','42A','42B'
-        ,'45A','45B','47A','47B','46A','46B','44A','44B','44C','48A','48B'],
+        beds: ['2', '4', '6', '8A', '8B', '1A', '1B', '10A', '10B', '3A',
+        '3B', '12A', '12B', '5A', '5B', '14A', '14B', '7A', '7B', '16A', '16B',
+        '9A', '9B', '18A', '18B', '11A', '11B', '20A', '20B', '22A', '22B', '22C',
+        '22D', '15A', '15B', '15C', '17A', '17B', '19A', '19B', '21A', '21B', '24A',
+        '24B', '24C', '23A', '23B', '26A', '26B', '26C', '25A', '25B', '27A', '27B',
+        '29A', '29B', '31A', '31B', '28A', '28B', '28C', '30A', '30B', '33A', '33B',
+        '32A', '32B', '35A', '35B', '34A', '34B', '37A', '37B', '36A', '36B', '39A',
+        '39B', '38A', '38B', '41A', '41B', '40A', '40B', '43A', '43B', '42A', '42B',
+        '45A', '45B', '47A', '47B', '46A', '46B', '44A', '44B', '44C', '48A', '48B'],
         onduty: [],
         occupied: {},
         census: 98,
         view: '',
         emptyBeds: {},
         assignment: [],
-        nurses: data
+        nurses: data,
       });
     });
   }
@@ -174,32 +187,34 @@ class App extends Component {
       case 'nurses':
         return (
           <div>
-            <Input enter={this.enter}/>
+            <Input enter={this.enter} />
             <Nurses
               nurses={this.state.nurses}
-              select={this.select}/>
+              select={this.select}
+            />
           </div>
         );
       case 'assign':
         return (
           <div>
-            <Input enter={this.enter}/>
-            <Assign assignment={this.state.assignment} nurses={this.state.onduty}/>
+            <Input enter={this.enter} />
+            <Assign assignment={this.state.assignment} nurses={this.state.onduty} />
           </div>
         );
       case 'display':
         return (
           <div>
-            <Input enter={this.enter}/>
+            <Input enter={this.enter} />
             <Display
               emptyBeds={this.state.emptyBeds}
-              census={this.state.census}/>
+              census={this.state.census}
+            />
           </div>
         );
       default:
         return (
           <div>
-            <Input enter={this.enter}/>
+            <Input enter={this.enter} />
           </div>
         );
     }
