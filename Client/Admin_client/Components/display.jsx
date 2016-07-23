e700dd2ca28d7f3f72a39e16594d324666ee4366
Chandra Lindy <chandra.lindy@gmail.com>
2016-07-23 15:38:28 -0700

    styling display view, add display view to admit and discharge functionality

--- Client/Admin_client/Components/display.jsx
+++ Client/Admin_client/Components/display.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Display extends Component {
-
+// props emptyBeds, occupied
   render() {
+    const census = this.props.occupied.length;
     return (
       <div className="container">
-          <div className="container-2col">
-            <h2>Today's empty beds:</h2>
+          <div className="container-left">
+            <div className="header">Today's empty beds:</div>
               {this.props.emptyBeds.map((el, i) => {
                 return (
-                  <span className="room" key={ i }>{ el }</span>
+                  <span className="room" key={i}>{el}</span>
                 );
               })}
           </div>
-          <div className="container-2col">
-            <h2>Today's Census: <span id="census">{ this.props.census }</span></h2>
+          <div className="container-right">
+            <div className="header">Census:</div> <span className="large-font">{census}</span>
           </div>
       </div>
     );
